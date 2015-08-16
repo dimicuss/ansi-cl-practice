8f4d8707d11973565d0b241773089181c56e5d05
dimicuss <dimicuss@gmail.com>
2015-08-16 19:31:04 +0300

    update

--- other/waterjug.lisp
+++ other/waterjug.lisp
@@ -1,23 +1,19 @@
 (defparameter *maxA* 8)
 (defparameter *maxB* 3)
-(defparameter *funcs* '(a->b b->a fillA fillB emptyA emptyB))
+(defparameter *funcs* '(pourA pourB fillA fillB emptyA emptyB))
 (defparameter *states* '((0 . 0)))
 
 (defun pourA (state)
   (let* ((a (car state)) (b (cdr state)) (perm (- *maxA* a)))
-    (if (or (= b 0) (= a *maxA*))
-	state
-	(if (<= b perm)
-	    (cons (+ a b) 0)
-	    (cons (+ a perm) (- b perm))))))
+    (cond ((or (= b 0) (= a *maxA*)) state)
+	  ((<= b perm) (cons (+ a b) 0))
+	  (t  (cons (+ a perm) (- b perm))))))
 
 (defun pourB (state)
   (let* ((a (car state)) (b (cdr state)) (perm (- *maxB* b)))
-    (if (or (= a 0) (= b *maxB*))
-	state
-	(if (<= a perm)
-	    (cons 0 (+ a b))
-	    (cons (- a perm) (+ b perm))))))
+    (cond ((or (= a 0) (= b *maxB*)) state)
+	  ((<= a perm) (cons 0 (+ a b)))
+	  (t (cons (- a perm) (+ b perm))))))
 
 (defun fillA (state)
   (cons *maxA* (cdr state)))
@@ -45,20 +41,4 @@
 	(return *states*)))))
 
 (solve 7)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
