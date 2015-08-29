2728d151a6180f18bc52c8ba4aac0a31251c3577
dimicuss <dimicuss@gmail.com>
2015-08-29 16:46:34 +0300

    update

--- part10.lisp
+++ part10.lisp
@@ -41,3 +41,37 @@
 (let ((n 2))
   (nth-expr n (/ 1 0) (+ 1 2) (/ 1 0))) ;;=> 3
 
+
+
+
+;;4. Определите ntimes (стр. 175), раскрывающуюся в (локальную) рекурсивную 
+;;   фнкцию.
+
+(defmacro n-times (n &body body)
+  (let ((gsym (gensym))
+	(gname (gensym)))
+    `(labels ((,gname (,gsym)
+		(unless (zerop ,gsym)
+		  ,@body
+		  (,gname (1- ,gsym)))))
+       (,gname ,n))))
+
+(let ((x 10))
+  (n-times x (setf x (+ x 1)))
+  x) ;;=> 20
+
+
+
+
+;;5. 
+
+(defmacro n-of (n exp)
+  (let ((fname (gensym)) (fvar (gensym)))
+    `(labels ((,fname (,fvar)
+		(if (zerop ,fvar)
+		    nil
+		    (cons ,exp (,fname (1- ,fvar))))))
+       (,fname ,n))))
+
+(let ((i 0) (n 4))
+  (n-of n (incf i))) ;;=> (1 2 3 4)
