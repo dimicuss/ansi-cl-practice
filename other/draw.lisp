24478f2eb2163721ab442a0804e2c6aa926530d1
dimicuss <dimicuss@gmail.com>
2015-09-01 23:46:24 +0300

    update

--- other/draw.lisp
+++ other/draw.lisp
@@ -1,20 +1,28 @@
 (ql:quickload :cl-cairo2)
 (in-package cl-cairo2)
 
-(defmacro with-pdf-context (args &rest body)
-  `(let ((*context* (create-pdf-context ,(first args) 
+(defmacro with-svg (args &rest body)
+  `(let ((*context* (create-svg-context ,(first args) 
 					,(second args)
 					,(third args))))
      ,@body
      (destroy *context*)))
 
-(with-pdf-context ("slayer.pdf" 200 100)
-  (set-source-rgb 0.2 0.2 1)
+(defun new-line (sx sy ex ey)
+  (new-sub-path)
+  (move-to sx sy)
+  (line-to ex ey))
+
+(with-svg ("slayer.svg" 200 200)
+  (set-source-rgb 0 0 0)
   (paint)
-  (move-to 200 0)
-  (line-to 0 100)
-  (set-source-rgb 1 1 1)
-  (set-line-width 5)
+  (arc 100 100 90 0 (* 2 pi))
+  (new-line 100 190 30 1)
+  (new-line 100 190 170 1)
+  (new-line 40 30 199 130)
+  (new-line 160 30 1 130)
+  (set-source-rgb 1 0 0)
+  (set-line-width 6)
   (stroke))
   
 
