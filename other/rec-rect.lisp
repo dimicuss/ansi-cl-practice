52e4f7f8068cfebdaefe766e4ba5abcf5f2504e7
Dmitry <dimicuss@gmail.com>
2015-09-24 12:46:56 +0400

    Update rec-rect.lisp

--- other/rec-rect.lisp
+++ other/rec-rect.lisp
@@ -12,15 +12,15 @@
     (draw-rectangle-* crCx crCy (ceiling a 2) (ceiling b 2))
     (draw-rectangle-* crDx crDy (ceiling a 2) (ceiling b 2))
     
-    (draw-rects crAx crAy (ceiling a 2) (ceiling b 2))
-    (draw-rects crBx crBy (ceiling a 2) (ceiling b 2))
-    (draw-rects crCx crCy (ceiling a 2) (ceiling b 2))
-    (draw-rects crDx crDy (ceiling a 2) (ceiling b 2))))
+    (draw-rects crAx crAy (ceiling a 2) (ceiling b 2) (- n 1))
+    (draw-rects crBx crBy (ceiling a 2) (ceiling b 2) (- n 1))
+    (draw-rects crCx crCy (ceiling a 2) (ceiling b 2) (- n 1))
+    (draw-rects crDx crDy (ceiling a 2) (ceiling b 2) (- n 1))))
     
     
     
 (draw-rectangle-* 0 900 900 900)
-(draw-cects 0 900 900 900)
+(draw-cects 0 900 900 900 5)
 
 
     
